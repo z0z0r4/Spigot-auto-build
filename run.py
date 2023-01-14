73e9889e51c2595ef0f0464b5f3bd98dec86a00a
z0z0r4 <z0z0r4@outlook.com>
2023-01-14 19:35:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -56,12 +56,48 @@ async def setup_java(version: int):
     else:
         print(f"Not found {version}")
         
+def get_bukit_version():
+    res = _get("https://hub.spigotmc.org/versions")
+    if res.status_code == 200:
+        text = res.text
+    else:
+        return None
+    version_list = re.findall('<a href="(1\..+?).json">',text)
+    return version_list
+
+def choose_java_version(oldversion: int, newversion: int) -> int:
+    for JavaVersion in JavaMajorVersion:
+        if JavaVersion <= newversion and JavaVersion >= oldversion:
+            return JavaMajorVersion[JavaVersion]
+    else:
+        return None
+
 def get_buildtool():
     with open("buildtools.jar", "wb") as f:
         f.write(_get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar").content)
-    print("Get BuildTools.jar")
-    os.system(f'{os.path.join(os.getenv(choose_java_version(61, 63)), "bin", "java")} -jar buildtools.jar --help')
-    
-if __name__ == "__main__":
-    # print(os.environ["JAVA_HOME"])
+    print('::set-output name=test_report::Get BuildTools.jar')
+
+# def build(version: str, java_path: str):
+#     os.system()
+
+def write_start_sh(command: str):
+    with open("start.sh", "w") as f:
+        f.write(f'#!/bin/bash\n{command}')
+
+def main():
+    bukkit_version = get_bukit_version()
+    print(f'::set-output name=test_report::{bukkit_version}')
     get_buildtool()
+    # for version in bukkit_version:
+    #     version_info = _get(f"https://hub.spigotmc.org/versions/{version}.json").json()
+    #     if version_info["javaVersions"] in MinecraftVersion:
+    #         # build(version, os.path.join(os.getenv(choose_java_version(version_info["javaVersions"][0], version_info["javaVersions"][1])), "bin", "java"))
+    version_info = _get(f"https://hub.spigotmc.org/versions/1.19.3.json").json()
+    java_path = os.path.join(os.getenv(choose_java_version(version_info["javaVersions"][0], version_info["javaVersions"][1])), "bin", "java")
+    write_start_sh(f'{os.path.join(java_path, "bin", "java")} -jar buildtools.jar --rev 1.19.3 --output-dir achieved')
+
+if __name__ == "__main__":
+    # asyncio.run(setup_java(52))
+    # print(os.environ["JAVA_HOME"])
+    # get_buildtool()
+    main()
